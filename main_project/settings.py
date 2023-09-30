a2a6120de4764f487bacdcfc1d78347a12a6f400
AbubakrChan <89600478+AbubakrChan@users.noreply.github.com>
2023-09-30 15:09:07 +0500

    Update settings.py

--- main_project/settings.py
+++ main_project/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     #new
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    # "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = 'main_project.urls'
